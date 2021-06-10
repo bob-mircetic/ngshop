12f7b37237871290486163250901d32e5393d0ed
Mircetic <17263@nelnet.net>
2021-06-10 15:30:32 -0600

    eod

---
+++ src/app/dog-list/dog-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { DogsService } from '../dogs.service';
+import { Dog } from '../dog';
+
+@Component({
+  selector: 'dog-list',
+  templateUrl: './dog-list.component.html',
+  styleUrls: ['./dog-list.component.css']
+})
+export class DogListComponent implements OnInit {
+
+  dogs: Dog[];
+
+  constructor(private dogService: DogsService) { }
+
+  title2: string = 'Dog List';
+  theDogs: object[];
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/dogs.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, OnInit } from '@angular/core';
+import DOGS from './dogdata.json';
+import { Observable, of } from 'rxjs';
+import { Dog } from './dog';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DogsService {
+
+  private dogData = DOGS; 
+
+  all(): Observable<Dog[]> {
+    return of(this.dogData);
+  }
+
+  constructor() { }
+}
+         
